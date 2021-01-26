bd8d07fe263a1e381a29f1a2ecdb604b1703a67e
Arthur Olmos Wosniaki <arthur.wosniaki@totvs.com.br>
2021-01-25 22:53:29 -0300

    Codigo final 1a aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,57 @@
-import styled from 'styled-components'
+import Widget from "../src/components/Widget";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
+import QuizBackground from "../src/components/QuizBackground";
+import QuizLogo from "../src/components/QuizLogo";
+import styled from "styled-components";
+import db from "../db.json";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
+
+const BackgroundImage = styled.div`
+  flex: 1;
+  background-image: url(${db.bg});
+  background-size: "cover";
+  background-position: "center";
+`;
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground BackgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The Legend of Zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum...</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz da Galera</h1>
+
+            <p>Lorem ipsum...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="github.com/arthurolmos" />
+    </QuizBackground>
+  );
 }
